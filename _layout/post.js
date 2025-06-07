4f59349bdfcd3333f0d4b6b210c2d102c5000246
ballboyDev <yswgood0329@gmail.com>
2025-06-07 14:32:33 +0900

    20250607

--- _layout/post.js
+++ _layout/post.js
@@ -1,149 +1,165 @@
-const assets = (param) => {
-    const { assetsUrl, index, fold } = param
+const code = {
+    assets: (param) => {
+        const { assetsUrl, index, fold } = param
 
-    const html = `
-<link rel="icon" href="${assetsUrl}/img/bDev.ico">
-<link rel="stylesheet" href="${assetsUrl}/markdown.css">
-<link rel="stylesheet" href="${assetsUrl}/skin.css">
+        const html = `
+            <link rel="icon" href="${assetsUrl}/img/bDev.ico">
+            <link rel="stylesheet" href="${assetsUrl}/markdown.css">
+            <link rel="stylesheet" href="${assetsUrl}/skin.css">
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/atom-one-dark.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/highlight.min.js"></script>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/atom-one-dark.min.css">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/highlight.min.js"></script>
 
-<style>
-    #p-${index} {
-        color: rgb(76, 193, 237) !important;
-    }
-</style>
-<script id="data" type="application/json">
-{
-    "fold": ${JSON.stringify(fold)}
-}
-</script>
+            <style>
+                #p-${index} {
+                    color: rgb(76, 193, 237) !important;
+                }
+            </style>
+            <script id="data" type="application/json">
+            {
+                "fold": ${JSON.stringify(fold)}
+            }
+            </script>
 
-<script src="${assetsUrl}/skin.js"></script>
-    `
+            <script src="${assetsUrl}/skin.js"></script>`
 
-    return html
-}
+        return html
+    },
 
-const prev = (param) => {
-    const { url, prev } = param
+    prev: (param) => {
+        const { url, prev } = param
 
-    const html = `
-<a href="${url}/post/${prev}.html" class="prev post">
-    <img src="${url}/assets/img/prev.svg" alt="" srcset="">
-</a>
-    `
+        const html = `
+            <a href="${url}/post/${prev}.html" class="prev post">
+                <img src="${url}/assets/img/prev.svg" alt="" srcset="">
+            </a>`
 
-    return html
-}
-const next = (param) => {
-    const { url, next } = param
+        return html
+    },
+    next: (param) => {
+        const { url, next } = param
 
-    const html = `
-<a href="${url}/post/${next}.html" class="next post">
-    <img src="${url}/assets/img/next.svg" alt="" srcset="">
-</a>
-    `
+        const html = `
+            <a href="${url}/post/${next}.html" class="next post">
+                <img src="${url}/assets/img/next.svg" alt="" srcset="">
+            </a>`
 
-    return html
-}
+        return html
+    },
 
-const bookmark = (param) => {
+    bookmark: (param) => {
 
-    const html = param.reduce((a, b) => {
-        return `${a}\n${`
+        const html = param.reduce((a, b) => {
+            return `${a}\n${`
             <div class="title t${b.depth}">
                 <div><a href="#bm-${b.index}">${b.raw.replace('\n', '')}</a></div>
                 <p>•</p>
             </div>`}`
-    }, '')
+        }, '')
 
-    return `<div class="bookmark">${html}</div>`
-}
+        return `<div class="bookmark">${html}</div>`
+    },
 
-const page = (param) => {
-    const { data, tag } = param
+    comment: () => {
+        return `
+            <script src="https://utteranc.es/client.js"
+                repo="ballboyDev/personal-storage"
+                issue-term="pathname"
+                label="comments"
+                theme="github-dark"
+                crossorigin="anonymous"
+                async>
+            </script>`
+    },
 
-    const html = `
-<!DOCTYPE html>
-<html lang="en">
+    page: (param) => {
+        const { data, tag } = param
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-site-verification" content="jrh7qB1kgowvnvE0P3KqlhI-iNmw9CvGJF4dQsaCTAk" />
-    <title>${data?.title || '심심한 개발자의 취미 생활에 오신걸 환영합니다.'}</title>
+        const html = `
+            <!DOCTYPE html>
+            <html lang="en">
 
-    ${tag.assets}
-</head>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta name="google-site-verification" content="jrh7qB1kgowvnvE0P3KqlhI-iNmw9CvGJF4dQsaCTAk" />
+                <meta name="naver-site-verification" content="6fe49ccdb8613221b89ae2d580300c63a91af446" />
+                
+                ${tag.assets}
 
-<body>
-    <div id="wrapper">
-        <!-- side bar component -->
-        <div id="head-bar">
-            <img id="openNavi" class="img"
-                src="${data.url}/assets/img/profile.jpeg"
-                alt="" srcset="">
-            <div class="title">심심한 개발자의 취미생활</div>
-        </div>
-        <div id="side-bar">
-            <img id="closeNavi" class="close"
-                src="${data.url}/assets/img/close.svg"
-                alt="" srcset="">
-            <div class="profile">
-                <img class="img" src="${data.url}/assets/img/profile.jpeg" alt="" srcset="">
-                <p>심심한 개발자의 취미생활</p>
-                <p>환영합니다.</p>
-            </div>
-            <hr />
-            <h3 class="home"><a href="${data.url}${data.env === 'dev' ? '/index.html' : ''}">HOME</a></h3>
-            <hr />
-            
-            <h3 class="category" onclick="foldNavi()">카테고리</h3>
-            <div class="navi">
-                <ul class="root">
+                <title>${data?.title || '심심한 개발자의 취미 생활에 오신걸 환영합니다.'}</title>
+            </head>
 
-                    ${tag.navi}
+            <body>
+                <div id="wrapper">
+                    <!-- side bar component -->
+                    <div id="head-bar">
+                        <img id="openNavi" class="img"
+                            src="${data.url}/assets/img/profile.jpeg"
+                            alt="" srcset="">
+                        <div class="title">심심한 개발자의 취미생활</div>
+                    </div>
+                    <div id="side-bar">
+                        <img id="closeNavi" class="close"
+                            src="${data.url}/assets/img/close.svg"
+                            alt="" srcset="">
+                        <div class="profile">
+                            <img class="img" src="${data.url}/assets/img/profile.jpeg" alt="" srcset="">
+                            <p>심심한 개발자의 취미생활</p>
+                            <p>환영합니다.</p>
+                        </div>
+                        <hr />
+                        <h3 class="home"><a href="${data.url}${data.env === 'dev' ? '/index.html' : ''}">HOME</a></h3>
+                        <hr />
+                        
+                        <h3 class="category" onclick="foldNavi()">카테고리</h3>
+                        <div class="navi">
+                            <ul class="root">
 
-                </ul>
-            </div>
+                                ${tag.navi}
 
-            <div class="copyright">
-                <img class="img"
-                    src="${data.url}/assets/img/github-mark-white.svg"
-                    alt="" srcset="">
-                <a href="https://github.com/BallboyDev/ballboyDev.github.io" target="_blank">Designed by ballboyDev</a>
-            </div>
+                            </ul>
+                        </div>
 
-        </div>
+                        <div class="copyright">
+                            <img class="img"
+                                src="${data.url}/assets/img/github-mark-white.svg"
+                                alt="" srcset="">
+                            <a href="https://github.com/BallboyDev/ballboyDev.github.io" target="_blank">Designed by ballboyDev</a>
+                        </div>
 
-        <!-- contents component -->
-        <div id="contents">
+                    </div>
 
-            ${tag.bookmark}
-            
-            ${tag.prev}
-            
-            ${tag.next}
-            
-            <div class="main markdown-body">
+                    <!-- contents component -->
+                    <div id="contents">
 
-                ${tag.contents}
+                        ${tag.bookmark}
+                        
+                        ${tag.prev}
+                        
+                        ${tag.next}
+                        
+                        <div class="main markdown-body">
 
-            </div>
-        </div>
-    </div>
-</body>
+                            ${tag.contents}
 
-</html>
-    `
+                        </div>
 
-    return html
+                        ${tag.comment}
+
+                    </div>
+                </div>
+            </body>
+            </html>`
+
+        return html
+    }
 }
 
 const output = (param) => {
+
     const env = process.env.NODE_ENV
+    const [prev, next] = !!param?.link ? param.link.split('/') : [0, 0]
 
     const temp = {
         data: {
@@ -153,20 +169,21 @@ const output = (param) => {
             env: param.env
         },
         tag: {
-            assets: assets({
+            assets: code.assets({
                 assetsUrl: env === 'dev' ? `${process.cwd()}/_assets` : `${param.url}/assets`,
                 index: param.index,
                 fold: param.fold
             }),
-            prev: (param.prev !== 0) ? prev({ url: param.url, prev: param.prev }) : '',
-            next: (param.next !== 0) ? next({ url: param.url, next: param.next }) : '',
+            prev: (parseInt(prev) !== 0) ? code.prev({ url: param.url, prev: prev }) : '',
+            next: (parseInt(next) !== 0) ? code.next({ url: param.url, next: next }) : '',
             navi: param.navi.join('\n'),
-            bookmark: !!param?.bookmark ? bookmark(param.bookmark) : '',
+            bookmark: !!param?.bookmark ? code.bookmark(param.bookmark) : '',
             contents: param.contents,
+            comment: !!param?.comment ? code.comment() : ''
         }
     }
 
-    return page(temp)
+    return code.page(temp)
 }
 
 module.exports = {
